8fb9bb1490d15d3572c1d0be33495539da39a152
_sheetal <Sheetal.j@consultadd.com>
2022-02-28 09:32:18 +0530

    commit with new

---
+++ src/test/java/com/example/tula/TulaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tula;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TulaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
