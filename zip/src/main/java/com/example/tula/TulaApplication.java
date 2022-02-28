91ca68f7281331468cd571aaff411348e26eac88
_sheetal <Sheetal.j@consultadd.com>
2022-02-28 09:25:54 +0530

    first commit

---
+++ zip/src/main/java/com/example/tula/TulaApplication.java
@@ -0,0 +1,13 @@
+package com.example.tula;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TulaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TulaApplication.class, args);
+	}
+
+}
